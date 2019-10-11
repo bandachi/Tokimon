0cd30089a75304f090aaeb3a2ed980b678d631cd
Hyeonseob Yang <nick_yang@sfu.ca>
2019-10-10 17:21:02 -0700

    Fixed heroku database

--- index.js
+++ index.js
@@ -13,4 +13,16 @@ express()
   .set('view engine', 'html')
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.sendFile('calculator.html', { root : VIEWS }))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
