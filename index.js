924a659b73feb914c05808cf9de6a133b1ba0cd4
Hyeonseob Yang <nick_yang@sfu.ca>
2019-10-13 01:58:16 -0700

    Added add and delete features

--- index.js
+++ index.js
@@ -12,30 +12,81 @@ express()
   .set('views', VIEWS)
   .set('view engine', 'ejs')
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('information'))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .get('/', (req, res) => {
+    var tokiQuery = `Select * FROM tokimon`;
+
+    pool.query(tokiQuery, (error, result) => {
+      if (error) {
+        res.end(error);
+      }
+      var results = {'rows':result.rows};
+      res.render('pages/information', results)
+    })
+  })
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM tokimon');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('db', results );
+      res.render('pages/db', results );
       client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
   })
-  .get('/tokimon/:name', (req, res) => {
-    var tokiNameQuery = `Select * FROM tokimon WHERE name='${req.params.name}'`;
-    //console.log(tokiNameQuery);
+  .get('/add', async (req, res) => {
+    res.render('pages/add');
+  })
+  .post('/add', (req, res) => {
+    console.log(req.body);
 
-    pool.query(tokiNameQuery, (error, result) => {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var fly = parseInt(req.body.fly);
+    var fight = parseInt(req.body.fight);
+    var fire = parseInt(req.body.fire);
+    var water = parseInt(req.body.water);
+    var electric = parseInt(req.body.electric);
+    var ice = parseInt(req.body.ice);
+
+    var total = fly + fight + fire + water + electric + ice;
+    console.log(`INSERT INTO tokimon VALUES (DEFAULT, '${req.body.tokiName}',
+      ${weight}, ${height}, ${fly}, ${fight}, ${fire}, ${water},  ${electric},
+      ${ice}, ${total}, '${req.body.trainer}')`);
+
+    var tokiIDQuery = `INSERT INTO tokimon VALUES (DEFAULT,
+      '${req.body.tokiName}', ${weight}, ${height}, ${fly}, ${fight}, ${fire},
+      ${water},  ${electric}, ${ice}, ${total}, '${req.body.trainer}')`;
+      
+    pool.query(tokiIDQuery, (error, result) => {
       if (error) {
         res.end(error);
       }
-      //console.log(result.rows);
+    })
+    res.end();
+  })
+  .get('/tokimon/:id', (req, res) => {
+    var tokiIDQuery = `SELECT * FROM tokimon WHERE id=${req.params.id}`;
+
+    pool.query(tokiIDQuery, (error, result) => {
+      if (error) {
+        res.end(error);
+      }
       var results = {'rows':result.rows[0]};
       res.render('pages/tokimon', results)
     })
+  })
+  .post('/delete/:id', (req, res) => {
+    console.log(req.params.id);
+    var tokiIDQuery = `DELETE FROM tokimon WHERE id=${req.params.id}`;
+    pool.query(tokiIDQuery, (error, result) => {
+      if (error) {
+        res.end(error);
+      }
+    })
+    res.end();
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
